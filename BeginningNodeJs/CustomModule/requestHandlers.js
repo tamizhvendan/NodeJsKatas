e50e16abaebdb78f4d77c9de8421339ee43e54fc
Tamizhvendan S <tamizh88@gmail.com>
2012-03-27 09:25:23 +0530

    Upload form added

--- BeginningNodeJs/CustomModule/requestHandlers.js
+++ BeginningNodeJs/CustomModule/requestHandlers.js
@@ -1,14 +1,24 @@
-var exec = require("child_process").exec;
+//var exec = require("child_process").exec;
+var fs = require('fs');
 
 function start(response){    
     console.log("Request for start invoked");    
-    exec("dir", function(error, stdout, stderr){
-        response.writeHead(200, { 'Content-Type' : 'text/plain' });
-        response.end(stdout);
-    });
+    //exec("dir", function(error, stdout, stderr){
+        //response.writeHead(200, { 'Content-Type' : 'text/plain' });
+        //response.end(stdout);
+    //});
+    fs.readFile("UploadForm.html", function(isReadError, contents){
+        if(!isReadError){
+            response.writeHead(200, {
+                "Content-Type" : "text/html",
+                "Content-Length" : contents.length
+            });
+            response.end(contents);
+        }
+    });    
 }
 
-function upload(){
+function upload(response){
     console.log("Request for upload invoked"); 
     response.writeHead(200, { 'Content-Type' : 'text/plain' });
     response.end("Work In Progress!!");
