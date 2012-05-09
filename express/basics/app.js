ed06b13b6b762a4d212c04ec114a291fad5f9608
Tamizhvendan S <tamizh88@gmail.com>
2012-05-09 15:18:01 +0530

    Added app-config for express basic samples

--- express/basics/app.js
+++ express/basics/app.js
@@ -1,3 +1,5 @@
+process.env.NODE_ENV = 'development';
+
 var express = require('express'),
     app = express.createServer();
 
@@ -5,8 +7,18 @@ app.configure(function(){
     app.use(express.logger());
 });
 
+app.configure('production', function(){
+	app.use(express.errorHandler());
+});
+
+app.configure('development', function(){
+	app.use(express.errorHandler({
+		dumpExceptions : true, showStack : true
+	}));
+});
+
 app.get('/', function(req, res){
-    res.send('Hello World');
+	throw new Error('An unexpected Error!');
 });
 
 app.listen(2000);
